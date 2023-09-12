0d1bfda4cc5f15ca00ced06aa923d3dfe1062d7d
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2023-09-12 12:33:38 -0600

    Tabla de Pronósticos: (Results) Encabezado mostrar logotipos en lugar de nombre "alias" de los equipos

--- resources/views/livewire/results/header_game_teams.blade.php
+++ resources/views/livewire/results/header_game_teams.blade.php
@@ -1,16 +1,16 @@
 <thead class="thead">
     <tr class="bg-dark text-white text-center text-sm">
         {{-- <th>Id</th> --}}
-        <th rowspan="2" valign="middle">PARTICIPANTE</th>
+        <th rowspan="2" valign="middle" class="w-40">PARTICIPANTE</th>
         @foreach ($round_games as $game)
-            <td><label  class="{{ $game->winner == 2 ? 'font-bold text-base' : '' }}"> {{ $game->visit_team->short }}</label>
-                -
-                <label  class="{{ $game->winner == 1 ? 'font-bold text-base' : '' }}"> {{ $game->local_team->short }}</label>
+            <td class="text-left">
+                <img src="{{Storage::url($game->visit_team->logo)}}" class="avatar-xs">
+                <img src="{{Storage::url($game->local_team->logo)}}" class="avatar-xs">
             </td>
         @endforeach
         <th rowspan="2" valign="middle">ACIERTOS</th>
     </tr>
-    <tr class="bg-dark text-white text-center text-sm">
+    <tr class="bg-dark text-white text-left text-sm">
         @foreach ($round_games as $game)
         <td>
             @if($game->visit_points || $game->local_pints)
