5464414687c8afdf3a759c69d0c79bec3fecaa30
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2023-10-05 15:15:13 -0600

    Menú administrador se agrega botón con menú desplegable para los CATALOGOS

--- resources/views/menus/admin.blade.php
+++ resources/views/menus/admin.blade.php
@@ -1,39 +1,79 @@
 @role('Admin')
-    @if(env('MOSTRAR_MENU_USUARIOS',false))
-        <x-nav-link href="{{ route('users') }}" :active="request()->routeIs('users')">
-            Usuarios
+
+        <div class="mt-3 relative">
+            <x-dropdown align="right" width="30">
+
+                <x-slot name="trigger">
+                    <span class="inline-flex rounded-md">
+                        <button type="button"
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                            CATALOGOS
+
+                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                            </svg>
+                        </button>
+                    </span>
+                </x-slot>
+
+                <x-slot name="content">
+                    <div class="w-30">
+                        <!-- Usuarios-->
+                        @if(env('MOSTRAR_MENU_USUARIOS',false))
+                            <x-dropdown-link href="{{ route('users') }}">
+                                Usuarios
+                            </x-dropdown-link>
+                        @endif
+
+                        <!-- Confituración General-->
+                        @if(env('MOSTRAR_MENU_CONFIGURACION',false))
+                            <x-dropdown-link href="{{ route('configurations') }}">
+                                Configuración
+                            </x-dropdown-link>
+                        @endif
+
+
+                        @if(env('MOSTRAR_MENU_ENTIDADES',false))
+                            <x-dropdown-link href="{{ route('entidades') }}">
+                                Entidades
+                            </x-dropdown-link>
+                        @endif
+
+                        @if (env('MOSTRAR_MENU_MUNICIPIOS',false))
+                            <x-dropdown-link href="{{ route('municipios') }}">
+                                Municipios
+                            </x-dropdown-link>
+                        @endif
+
+                        @if(env('MOSTRAR_MENU_JORNADAS',false))
+                            <x-dropdown-link href="{{ route('rounds') }}">
+                                Jornadas
+                            </x-dropdown-link>
+                        @endif
+
+
+                        @if(env('MOSTRAR_MENU_EQUIPOS',false))
+                            <x-dropdown-link href="{{ route('teams') }}">
+                                Equipos
+                            </x-dropdown-link>
+                        @endif
+
+                    </div>
+                </x-slot>
+            </x-dropdown>
+        </div>
+
+
+
+    @if (Route::has('positions-by-round'))
+        <x-nav-link href="{{ route('positions-by-round') }}" :active="request()->routeIs('positions-by-round')">
+            Posiciones por Jornada
         </x-nav-link>
     @endif
 
-    @if(env('MOSTRAR_MENU_CONFIGURACION',false))
-        <x-nav-link href="{{ route('configurations') }}" :active="request()->routeIs('configurations')">
-            Configuración
-        </x-nav-link>
-    @endif
-
-
-    @if(env('MOSTRAR_MENU_ENTIDADES',false))
-        <x-nav-link href="{{ route('entidades') }}" :active="request()->routeIs('entidades')">
-            Entidades
-        </x-nav-link>
-    @endif
-
-    @if (env('MOSTRAR_MENU_MUNICIPIOS',false))
-        <x-nav-link href="{{ route('municipios') }}" :active="request()->routeIs('municipios')">
-            Municipios
-        </x-nav-link>
-    @endif
-
-    @if(env('MOSTRAR_MENU_JORNADAS',false))
-        <x-nav-link href="{{ route('rounds') }}" :active="request()->routeIs('rounds')">
-            Jornadas
-        </x-nav-link>
-    @endif
-
-
-    @if(env('MOSTRAR_MENU_EQUIPOS',false))
-        <x-nav-link href="{{ route('teams') }}" :active="request()->routeIs('teams')">
-            Equipos
+    @if (Route::has('positions-general'))
+        <x-nav-link href="{{ route('positions-general') }}" :active="request()->routeIs('positions-general')">
+            Posiciones Generales
         </x-nav-link>
     @endif
 
