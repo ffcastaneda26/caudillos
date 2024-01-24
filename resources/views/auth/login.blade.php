03be15c321ac15d62d40df9f06cac0a472075a1e
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-01-24 17:49:50 -0600

    Agregar imagen de patrocinador abajo del formulario de ingreso

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <x-guest-layout>
     <x-authentication-card>
+
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
@@ -12,46 +13,54 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+        <div>
+            <form method="POST" action="{{ route('login') }}" class="mt-4">
+
+                @csrf
+                <div>
+                    <x-label class="texto-azul" for="email" value="{{ __('Email') }}" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                        autofocus autocomplete="username" />
+                </div>
+                @include('auth.login_button')
+
+                <div class="block mt-4">
+                    <label for="remember_me" class="flex items-center">
+                        <x-checkbox id="remember_me" name="remember" checked class="invisible"/>
+                    </label>
+                </div>
+
+                <div class="flex flex-col">
+
+                    @if (Route::has('password.request'))
+                        <div class="text-right">
+                            <a class="underline texto-oro text-xs font-semibold dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                                href="#">
+                                {{ __('Forgot your password?') }}
+                            </a>
+                        </div>
+                    @endif
+
+                    <div class="flex items-center justify-between mt-4">
+                        <a href="{{ route('register') }}"
+                            class="inline rounded-xl btn btn-sm fondo-secundario texto-azul font-semibold  hover:text-white">
+                            {{ __('Register') }}
+                        </a>
+
+                        <x-login />
+                    </div>
+
+                </div>
 
 
             <div>
-                <x-label class="texto-azul" for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                    autofocus autocomplete="username" />
-            </div>
-
-            @include('auth.login_button')
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" checked/ class="invisible">
-                </label>
-            </div>
 
 
-            <div class="flex flex-col">
-                {{-- href="{{ route('password.request') }}" --}}
-                @if (Route::has('password.request'))
-                    <div class="text-right">
-                        <a class="underline texto-oro text-xs font-semibold dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                            href="#">
-                            {{ __('Forgot your password?') }}
-                        </a>
-                    </div>
-                @endif
+            </form>
+        </div>
 
-                <div class="flex items-center justify-between mt-4">
-                    <a href="{{ route('register') }}"
-                        class="inline rounded-xl btn btn-sm fondo-secundario texto-azul font-semibold  hover:text-white">
-                        {{ __('Register') }}
-                    </a>
-
-                    <x-login />
-                </div>
-
-            </div>
-        </form>
     </x-authentication-card>
+    {{-- Logo de patrocinador principal --}}
+    <x-authentication-card-partner-logo />
+
 </x-guest-layout>
--- resources/views/auth/login_button.blade.php
+++ resources/views/auth/login_button.blade.php
@@ -3,11 +3,11 @@
         <div>
             <x-label for="password" value="{{ __('Password') }}" />
             <div class="flex items-center">
-                <x-input id="password" x-bind:type="showPassword ? 'text' : 'password'" name="password"
-                    autocomplete="new-password" class="block mt-1 w-full" />
-                @error('password')
-                    <div class="badge rounded-pill bg-danger">{{ $message }}</div>
-                @enderror
+                <x-input id="password"
+                        x-bind:type="showPassword ? 'text' : 'password'"
+                        name="password"
+                        class="block mt-1 w-full" />
+
                 <button x-on:click="showPassword = !showPassword" type="button"
                     class="ml-2 flex items-center">
                     <i class="fa-solid fa-eye"></i>
