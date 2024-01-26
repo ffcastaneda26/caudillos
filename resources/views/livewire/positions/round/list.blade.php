599eb1ec49f12ca06d802e56da1ce58c7bef919b
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-01-26 01:20:18 -0600

    Lista de posiciones por jornada

--- resources/views/livewire/positions/round/list.blade.php
+++ resources/views/livewire/positions/round/list.blade.php
@@ -1,8 +1,8 @@
 <tr>
-    {{-- @dd($position) --}}
+
     <td>{{ $position->position }}</td>
     <td>{{ $position->name }}</td>
-    <td align="center">{{ $position->hits }}</td>
+    <td align="center">{{ $position->hits ?  $position->hits : '-'}}</td>
 
     @if ($tie_breaker_game_played)
         <td align="center">
@@ -14,13 +14,19 @@
         <td align="center">{{ $position->dif_winner_points }}</td>
         <td align="center">{{ $position->dif_victory }}</td>
     @else
-        <td>
-            @if ($position->tie_break_winner == 1)
-                <img src="{{ Storage::url($tie_breaker_game->local_team->logo) }}" class="avatar-xs md:w-100 h-100">
-            @else
-                <img src="{{ Storage::url($tie_breaker_game->visit_team->logo) }}" class="avatar-xs md:w-100 h-100">
-            @endif
-        </td>
-        <td>{{ $position->tie_break_visit_points . '-' . $position->tie_break_local_points }}</td>
+        @if ($position->tie_break_winner)
+            <td>
+                @if ($position->tie_break_winner == 1)
+                    <img src="{{ Storage::url($tie_breaker_game->local_team->logo) }}" class="avatar-xs md:w-100 h-100">
+                @else
+                    <img src="{{ Storage::url($tie_breaker_game->visit_team->logo) }}" class="avatar-xs md:w-100 h-100">
+                @endif
+            </td>
+            <td>{{ $position->tie_break_visit_points . '-' . $position->tie_break_local_points }}</td>
+        @else
+            <td style="text-align: center;">
+                <x-reloj-image />
+            </th>
+        @endif
     @endif
 </tr>
