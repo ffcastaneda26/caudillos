178ae0c6be42bec41f5352b1c373e99fe11a4f57
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-02-02 13:26:44 -0600

    Cambio de contraseña en perfil alineado

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
 
-    <div class="text-center">
+    <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
                 @livewire('profile.update-profile-information-form')
@@ -12,6 +12,8 @@
                     <div class="col-md-5">
                         @livewire('data-users');
                     </div>
+
+                <x-section-border />
                     <div class="col-md-7">
                          <div class="mt-10 sm:mt-0">
                             @livewire('profile.update-password-form')
