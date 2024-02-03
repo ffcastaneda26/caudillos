682ad78093955605b0de4617cd2a39dcdded753a
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-02-03 11:38:05 -0600

    Lista de posiciones Generales: Se corriege variable tenía $points y debe ser $total_points

--- resources/views/livewire/positions/general/list.blade.php
+++ resources/views/livewire/positions/general/list.blade.php
@@ -1,6 +1,6 @@
 <tr>
     <td>{{ $position->position }}</td>
     <td>{{ $position->name }}</td>
-    <td align="center">{{ $position->points ?  $position->points : '-'}}</td>
+    <td align="center">{{ $position->total_points ?  $position->total_points : '-'}}</td>
     <td align="center">{{ $position->hits ?  $position->hits : '-'}}</td>
 </tr>
