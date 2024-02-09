b6278aa3fabe99ba860d374419be37c95a6512c9
Federico Castañeda Ortiz <ffcastaneda@gmail.com>
2024-02-08 18:04:47 -0600

    Tabla de pronóstios (Results) ruta results-by-round se pone marcadores uno arriba del otro

--- resources/views/livewire/results/header_game_teams.blade.php
+++ resources/views/livewire/results/header_game_teams.blade.php
@@ -13,6 +13,7 @@
                 <img src="{{ Storage::url($game->local_team->logo) }}" style="width: 24px">
             </td>
         @endforeach
+
         <th rowspan="2" valign="middle" class="text-xs">ACIERTOS</th>
     </tr>
     <tr class="fondo-azul text-white text-left text-xs">
@@ -20,7 +21,7 @@
             <td>
                 @if ($game->visit_points || $game->local_pints)
                     {{ $game->visit_points ? $game->visit_points : '0' }}
-                    -
+                    <br>
                     {{ $game->local_points ? $game->local_points : '0' }}
                 @endif
             </td>
